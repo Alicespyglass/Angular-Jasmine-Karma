fb83446ce8c4c7bddc85855531e0eb48e571294d
Alice Cheung <alice@myurbanjungle.com>
2017-12-22 10:43:38 +0000

    3 - tests for vote component

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,22 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  it('should increment total votes when upvoted', () => {
+      //   Arrange
+      let component = new VoteComponent();
+
+      //   Act
+      component.upVote();
+
+      //   Assert
+      expect(component.totalVotes).toBe(1);
   });
 
-  it('', () => {
+  it('should decrement total votes when downvoted', () => {
+      let component = new VoteComponent();
+
+      component.downVote();
+
+      expect(component.totalVotes).toBe(-1);
   });
 });

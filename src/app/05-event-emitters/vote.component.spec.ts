6073724d7b0228a4ea653c3f995f1c25492d4df7
Alice Cheung <alice@myurbanjungle.com>
2017-12-22 11:41:57 +0000

    5 - tests for Event Emitters

--- src/app/05-event-emitters/vote.component.spec.ts
+++ src/app/05-event-emitters/vote.component.spec.ts
@@ -1,12 +1,23 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  var component: VoteComponent; 
+  var component: VoteComponent;
 
   beforeEach(() => {
     component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should raise voteChanged event when upvoted', () => {
+    //   Arrange
+    let totalVotes = null;
+    component.voteChanged.subscribe(tv => totalVotes = tv) // voteChanged is an event so can be subscribed
+
+    // Act
+    component.upVote();
+
+    // Assert
+    expect(totalVotes).not.toBeNull(); // could pass even with a bug
+    expect(totalVotes).toBe(1); // better to be more specific in this case
+;
   });
 });

67f02462b3ec6744eafe517877311f54ccfcfdb7
Alice Cheung <alice@myurbanjungle.com>
2017-12-27 14:38:34 +0000

    6 - interactive testing

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -3,6 +3,8 @@ import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
 import { Observable } from 'rxjs/Observable'; // remember to import Observable
 import 'rxjs/add/observable/from';
+import 'rxjs/add/observable/empty';
+import 'rxjs/add/observable/throw';
 
 describe('TodosComponent', () => {
 // 1. declare component and service
@@ -18,7 +20,7 @@ describe('TodosComponent', () => {
 // 3. spy calls the getTodos function from the service and returns an Observable
 // from the array
   it('should set todos property with the items returned from the server', () => {
-    let todos = [1,2,3]
+    let todos = [1,2,3];
       // Arrange
       spyOn(service, 'getTodos').and.callFake(() => {
           return Observable.from([ todos ]);
@@ -30,4 +32,52 @@ describe('TodosComponent', () => {
       //Assert - expect todos back from server
       expect(component.todos).toBe(todos);
   });
+
+// 7 - Interactive Testing
+// 7a - a method is called on the service
+  it('should call the server to save the changes when a new todo item is added', () => {
+  // add() method takes in todo as a parameter and returns one in the Observable
+    let spy = spyOn(service, 'add').and.callFake(t => {
+        // remember to import empty method from Observable at the top of the file
+        return Observable.empty();
+    });
+
+    // Act
+    component.add();
+
+    // Assert
+    expect(spy).toHaveBeenCalled();
+  });
+
+  // 7b - a method is called on the service
+    it('should add the new todo returned from the server', () => {
+    // todo object returned from server
+    let todo = { id:1 };
+    // instead of callFake(), cleaner way is to returnValue and get rid of arrow function
+      let spy = spyOn(service, 'add').and.returnValue(Observable.from([ todo ]));
+
+      // Act
+      component.add();
+
+      // Assert
+      expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+    });
+
+    // 7c - if an error is returned, error is set to message property
+    it('should set the message property if server returns an error when adding a new todo', () => {
+    // declare an error message
+    let error = "error from the server";
+    let todo = { id:1 };
+    // create Observable from an error
+      let spy = spyOn(service, 'add').and.returnValue(Observable.throw(error));
+
+      // Act
+      component.add();
+
+      // Assert
+      expect(component.message).toBe(error);
+    });
+
+
+
 });
